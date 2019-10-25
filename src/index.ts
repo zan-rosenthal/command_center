c5f0665d317140402781385cbdf15cedfc46eb55
zan-rosenthal <zan.rosenthal@gmail.com>
2019-10-25 17:26:43 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import * as fastify from "fastify";
+import { Server, IncomingMessage, ServerResponse } from "http";
+import routes from "./routes";
+
+// Create a http server. We pass the relevant typings for our http version used.
+// By passing types we get correctly typed access to the underlying http objects in routes.
+// If using http2 we'd pass <http2.Http2Server, http2.Http2ServerRequest, http2.Http2ServerResponse>
+const server: fastify.FastifyInstance<
+  Server,
+  IncomingMessage,
+  ServerResponse
+> = fastify({ logger: true });
+
+server.register(routes);
+
+const start = async () => {
+  try {
+    await server.listen(3000, "0.0.0.0");
+  } catch (err) {
+    console.log(err);
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+process.on("uncaughtException", error => {
+  console.error(error);
+});
+process.on("unhandledRejection", error => {
+  console.error(error);
+});
+
+start();
